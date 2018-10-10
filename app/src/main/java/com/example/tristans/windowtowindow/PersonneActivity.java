82d2a4d8b1df8d7bb652efa053955822fbdb7f80
Theo Bouchevreau <theo.bouchevreau@viacesi.fr>
2018-10-10 09:34:05 +0200

    fin du tp

--- app/src/main/java/com/example/tristans/windowtowindow/PersonneActivity.java
+++ app/src/main/java/com/example/tristans/windowtowindow/PersonneActivity.java
@@ -2,6 +2,11 @@ package com.example.tristans.windowtowindow;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import classes.Personne;
 
 public class PersonneActivity extends Activity {
 
@@ -9,5 +14,10 @@ public class PersonneActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personne);
+        Personne d = this.getIntent().getParcelableExtra("personne");
+        TextView textPrenom = (TextView) findViewById(R.id.textViewPrenom);
+        TextView textNom = (TextView)findViewById(R.id.textViewNom);
+        textPrenom.setText("Prénom : "+d.getPrenom());
+        textNom.setText("Nom : "+d.getNom());
     }
 }
